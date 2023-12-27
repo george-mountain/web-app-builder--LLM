6e9360b00ea2c41e74e54acf8bba6535affb1b3e
george-mountain <georgejiokem@gmail.com>
2023-12-27 14:28:48 +0900

    package installer setup tool

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webapp_builder",
+    version="0.0.1",
+    author="George Mountain",
+    author_email="georgejiokem@gmail.com",
+    description="Webapp Builder -- LLM/LVM Code Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/george-mountain/web-app-builder--LLM.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/george-mountain/web-app-builder--LLM.git/issues"
+    },
+    license="MIT",
+    packages=["webapp_builder"],
+    install_requires=[
+        "requests",
+        "python-dotenv",
+        "langchain",
+        "transformers",
+        "openai",
+        "Pillow",
+        "streamlit",
+    ],
+)
